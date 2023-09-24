023dc147d587220c1b90f575b9e57cfad0f0addf
daniell.gm03 <daniell.gm03@gmail.com>
2023-09-24 23:51:01 +0200

    init develop

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:24.0.0")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+    implementation("org.xerial:sqlite-jdbc:3.43.0.0")
+    implementation("org.mybatis:mybatis:3.5.13")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
